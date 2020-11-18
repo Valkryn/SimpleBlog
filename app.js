494f5cee5fb56503a4cddc1c58251640a6aae61e
Valkryn <calvinraphaelg@gmail.com>
2020-11-18 17:15:28 -0500

    form added and bootstrap

--- app.js
+++ app.js
@@ -28,7 +28,14 @@ app.get("/contact",function(req,res){
   res.render("contact",{contactContent: contactContent})
 })
 
+app.get("/compose", function(req,res){
+  res.render("compose")
+})
 
+app.post("/compose",function(req,res){
+  const post = req.body
+  console.log(post)
+})
 
 
 app.listen(3000, function() {
